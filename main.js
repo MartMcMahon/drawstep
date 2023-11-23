2dc81f3c05415f1fc6dc3a2c99a94ae1ed360008
Mart <themcman@gmail.com>
2023-11-22 21:56:34 -0600

    cards have rounded corners

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const ShapeType = Object.freeze({
   Oval: 2,
 });
 
+// class Deck {
+//   constructor()
+// }
+
 class Shape {
   constructor(x, y, params) {
     Object.assign(this, { x, y, ...params });
@@ -16,8 +20,6 @@ class Shape {
     this.color = params.color || { r: 100, b: 100, g: 100, a: 1 };
     this.shape = params.shape || ShapeType.Diamond;
     this.fillType = params.fillType || FillType.Empty;
-
-    // console.log(this);
   }
 
   draw(ctx) {
@@ -40,6 +42,7 @@ class Shape {
     let color = `rgba(${r}, ${g}, ${b}, ${a})`;
     switch (this.fillType) {
       case FillType.Empty:
+        ctx.save();
         ctx.strokeStyle = color;
         ctx.stroke();
         break;
@@ -48,7 +51,13 @@ class Shape {
         ctx.fill();
         for (let i = 0; i < this.width * 2; i = i + this.width / 10) {
           ctx.clearRect(this.x + i, this.y, this.width / 20, this.height * 2);
+          ctx.beginPath();
+          ctx.rect(this.x + i, this.y, this.width / 20, this.height * 2);
+          ctx.closePath();
+          ctx.fillStyle = Card.backgroundColor;
+          ctx.fill();
         }
+        ctx.fillStyle = color;
         this.fillType = FillType.Empty;
         this.draw(ctx);
         this.ink(ctx);
@@ -68,33 +77,64 @@ class Shape {
 }
 
 class Card {
-  static padding_space = 10;
+  static width = 50;
+  static height = 80;
+  static spacer = 25;
+  static render_offset = 5;
+  static backgroundColor = "rgba(180, 180, 180, 1)";
 
   constructor(count, color, fillType, shape) {
     Object.assign(this, { count, color, fillType, shape });
     this.x = 0;
     this.y = 0;
 
-    let spacer = 30;
-    // console.log(this);
     this.shapeSprites = [];
     for (let i = 0; i < count; i++) {
       this.shapeSprites.push(
-        new Shape(this.x, this.y + i * spacer, { color, fillType, shape })
+        new Shape(this.x, this.y + i * Card.spacer, { color, fillType, shape })
       );
     }
   }
 
   setPos(x, y) {
-    let spacer = 30;
     Object.assign(this, { x, y });
     this.shapeSprites.forEach((shape, i) => {
-      shape.x = x;
-      shape.y = y + i * spacer;
+      shape.x = x + Card.render_offset;
+      shape.y = y + i * Card.spacer + Card.render_offset;
     });
   }
 
   draw(ctx) {
+    const NORTH = -0.5 * Math.PI;
+    const EAST = 0;
+    const SOUTH = 0.5 * Math.PI;
+    const WEST = Math.PI;
+
+    let cornRad = Card.height / 20;
+    ctx.beginPath();
+    let p = [
+      { x: this.x, y: this.y + cornRad },
+      { x: this.x + cornRad, y: this.y },
+      { x: this.x + Card.width - cornRad, y: this.y },
+      { x: this.x + Card.width, y: this.y + cornRad },
+      { x: this.x + Card.width, y: this.y + Card.height - cornRad },
+      { x: this.x + Card.width - cornRad, y: this.y + Card.height },
+      { x: this.x + cornRad, y: this.y + Card.height },
+      { x: this.x, y: this.y + Card.height - cornRad },
+    ];
+    ctx.moveTo(p[0].x, p[0].y);
+    ctx.arc(p[1].x, p[0].y, cornRad, WEST, NORTH);
+    ctx.lineTo(p[2].x, p[2].y);
+    ctx.arc(p[2].x, p[3].y, cornRad, NORTH, EAST);
+    ctx.lineTo(p[4].x, p[4].y);
+    ctx.arc(p[5].x, p[4].y, cornRad, EAST, SOUTH);
+    ctx.lineTo(p[6].x, p[6].y);
+    ctx.arc(p[6].x, p[7].y, cornRad, SOUTH, WEST);
+    ctx.closePath();
+    ctx.strokeStyle = `rgba(20, 20, 20, 1)`;
+    ctx.stroke();
+    ctx.fillStyle = Card.backgroundColor;
+    ctx.fill();
     this.shapeSprites.forEach((shape) => {
       shape.draw(ctx);
       shape.ink(ctx);
@@ -127,14 +167,13 @@ colors.forEach((color, c_index) => {
       let fill_x = fillType * 100;
       [1, 2, 3].forEach((count) => {
         let card = new Card(count, color, fillType, shapeType, {});
-        card.setPos(color_x + fill_x, shape_y + ( count-1 ) * 100);
+        card.setPos(color_x + fill_x, shape_y + (count - 1) * 100);
         console.log(card.x, card.y);
         card.draw(ctx);
-       });
+      });
     });
   });
 });
-
 
 function drawOval(ctx, x, y, color, fill_type) {
   let width = 40;
@@ -230,28 +269,4 @@ function drawDiamond(ctx, x, y) {
   ctx.lineTo(x + width, y + height / 2);
   ctx.lineTo(x + width / 2, y + height);
   ctx.closePath();
-}
-
-function ink(ctx, color, fill_type) {
-  let { r, g, b, a } = color;
-  color = `rgba(${r}, ${g}, ${b}, ${a})`;
-  switch (fill_type) {
-    case FillType.Empty:
-      ctx.strokeStyle = color;
-      ctx.stroke();
-      break;
-    // case FillType.Shade:
-    //   ctx.fillStyle = color;
-    //   ctx.fill();
-    //   break;
-    case FillType.Full:
-      ctx.strokeStyle = color;
-      ctx.stroke();
-      ctx.fillStyle = color;
-      ctx.fill();
-      break;
-    default:
-      console.error("invalid fill type");
-      break;
-  }
 }
